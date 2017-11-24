bb3e57d4c7cf1dd6b637360fe4b6ed7a16a22f32
asvirido <trotillostudio@gmail.com>
2017-11-24 13:07:05 +0200

    add file for Admin

--- exchange/Admin.sol
+++ exchange/Admin.sol
@@ -1,27 +1,5 @@
 pragma solidity ^0.4.16;
 
-contract Token {
-
-	uint public 	decimals;
-	string public 	name;
-	
-	function totalSupply() constant returns (uint256 supply);
-	
-	function balanceOf(address _owner) constant returns (uint256 balance);
-	
-	function transfer(address _to, uint256 _value) returns (bool success);
-	
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-	
-	function approve(address _spender, uint256 _value) returns (bool success);
-
-	function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-	
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-}
-
 contract Admin {
 	
 	address public	admin;
@@ -64,5 +42,3 @@ contract Admin {
 	function changeFeeRebate( uint feeRebate) adminValid public {
 		_feeRebate = feeRebate;
 	}
-	
-}
