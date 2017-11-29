3dbf6748cd2b6ff3b40821032baed8884937ed98
Antonio <trotillostudio@gmail.com>
2017-11-29 19:22:29 +0200

    Update test.sol

--- exchange/etheldelta/test.sol
+++ exchange/etheldelta/test.sol
@@ -4,7 +4,7 @@ contract SafeMath {
 
     function safeMul(uint a, uint b) internal returns (uint) {
         uint c = a * b;
-        //assert(a == 0 || c / a == b);
+        assert(a == 0 || c / a == b);
         return c;
     }
     
