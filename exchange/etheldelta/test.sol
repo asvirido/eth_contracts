d38d72912dba0ead2b22d09e64e17aaa27c857f6
asvirido <trotillostudio@gmail.com>
2017-12-12 16:05:03 +0200

    add new documentation

--- exchange/etheldelta/test.sol
+++ exchange/etheldelta/test.sol
@@ -53,10 +53,10 @@ contract Token {
 contract Admin {
 
 	address public	admin;
-	address public	feeAccount;
-	address public 	nextVersionAddress;
-	bool 	public	orderEnd;
-	string  public 	version;
+	address public	feeAccount; // address feeAccount, which will receive fee.
+	address public 	nextVersionAddress; // this is next address exchange
+	bool 	public	orderEnd; // this is var use when Admin want close exchange
+	string  public 	version; // number version example 1.0, test_1.0
 	uint 	public	feeTake; //percentage times (1 ether)
 
 	modifier assertAdmin() {
@@ -67,31 +67,51 @@ contract Admin {
 		_;
 	}
 
+	/*
+	*	This is function, is needed to change address admin.
+	*/
 	function setAdmin( address _admin ) assertAdmin public {
 		
 		admin = _admin;
 	}
 
+	/*
+	* 	This is function, is needed to change version smart-contract.
+	*/
 	function setVersion(string _version) assertAdmin public {
+		
 		version = _version;	
 	}
-	
 
+	/*
+	* 	This is function, is needed to set address, next smart-contracts.
+	*/
 	function setNextVersionAddress(address _nextVersionAddress) assertAdmin public{
 		
 		nextVersionAddress = _nextVersionAddress;	
 	}
 
+	/*
+	* 	This is function, is needed to stop, news orders.
+	*	Can not turn off it.
+	*/
 	function setOrderEnd() assertAdmin public {
 		
 		orderEnd = true;
 	}
 
+	/*
+	*	This is function, is needed to change address feeAccount.
+	*/
 	function setFeeAccount( address _feeAccount ) assertAdmin public {
 		
 		feeAccount = _feeAccount;
 	}
 
+	/*
+	* 	This is function, is needed to set new fee.
+	*	Can only be changed down.
+	*/
 	function setFeeTake( uint _feeTake ) assertAdmin public {
 		
 		if ( _feeTake > feeTake )
