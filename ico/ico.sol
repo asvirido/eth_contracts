4763654c3999b2f186e041a2bfb0e2d1d902d942
asvirido <trotillostudio@gmail.com>
2017-12-14 14:15:51 +0200

    deleted space in safeMath and Erc20

--- ico/ico.sol
+++ ico/ico.sol
@@ -1,26 +1,26 @@
 contract safeMath {
 
-    function add( uint256 x, uint256 y ) constant internal returns ( uint256 z ) {
-        assert( ( z = x + y ) >= x );
-    }
+	function add( uint256 x, uint256 y ) constant internal returns ( uint256 z ) {
+		assert( ( z = x + y ) >= x );
+	}
 
-    function sub( uint256 x, uint256 y ) constant internal returns ( uint256 z ) {
-        assert( ( z = x - y ) <= x );
-    }
+	function sub( uint256 x, uint256 y ) constant internal returns ( uint256 z ) {
+		assert( ( z = x - y ) <= x );
+	}
 }
 
 contract ERC20 {
-    
-    function 	totalSupply() constant returns ( uint supply );
-    function 	balanceOf( address who ) constant returns ( uint value );
-    function 	allowance( address owner, address spender ) constant returns ( uint _allowance );
 
-    function 	transfer( address to, uint value ) returns ( bool ok );
-    function 	transferFrom( address from, address to, uint value) returns ( bool ok );
-    function 	approve( address spender, uint value ) returns ( bool ok );
+	function 	totalSupply() constant returns ( uint supply );
+	function 	balanceOf( address who ) constant returns ( uint value );
+	function 	allowance( address owner, address spender ) constant returns ( uint _allowance );
 
-    event 		Transfer( address indexed from, address indexed to, uint value );
-    event 		Approval( address indexed owner, address indexed spender, uint value );
+	function 	transfer( address to, uint value ) returns ( bool ok );
+	function 	transferFrom( address from, address to, uint value) returns ( bool ok );
+	function 	approve( address spender, uint value ) returns ( bool ok );
+
+	event 		Transfer( address indexed from, address indexed to, uint value );
+	event 		Approval( address indexed owner, address indexed spender, uint value );
 }
 
 contract 	baseToken is ERC20, safeMath {
