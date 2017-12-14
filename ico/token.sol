35472cff4ce610a7a077670fb078a873722c453e
asvirido <trotillostudio@gmail.com>
2017-12-14 12:50:13 +0200

    update style erc20 and etc

--- ico/token.sol
+++ ico/token.sol
@@ -1,5 +1,35 @@
 pragma solidity ^0.4.16;
 
+contract ERC20Interface {
+
+	// Get the total token supply
+	function 	totalSupply() constant returns ( uint256 totalSupply );
+
+	// Get the account balance of another account with address _owner
+	function 	balanceOf( address _owner ) constant returns ( uint256 balance );
+
+	// Send _value amount of tokens to address _to
+	function 	transfer( address _to, uint256 _value ) returns ( bool success );
+
+	// Send _value amount of tokens from address _from to address _to
+	function 	transferFrom( address _from, address _to, uint256 _value ) returns ( bool success );
+
+	// Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+	// If this function is called again it overwrites the current allowance with _value.
+	// this function is required for some DEX functionality
+	function 	approve( address _spender, uint256 _value ) returns ( bool success );
+
+	// Returns the amount which _spender is still allowed to withdraw from _owner
+	function 	allowance( address _owner, address _spender ) constant returns ( uint256 remaining );
+
+	// Triggered when tokens are transferred.
+	event 		Transfer( address indexed _from, address indexed _to, uint256 _value );
+
+	// Triggered whenever approve(address _spender, uint256 _value) is called.
+	event 		Approval( address indexed _owner, address indexed _spender, uint256 _value );
+}
+
+
 interface tokenRecipient{
 
 	function receiveApproval( address _from, uint256 _value, address _token, bytes _extraData ) public;
@@ -17,11 +47,8 @@ contract TokenERC20 {
 	mapping ( address => uint256 ) public balanceOf;
 	mapping ( address => mapping ( address => uint256 ) ) public allowance;
 
-	// This generates a public event on the blockchain that will notify clients
-	event Transfer( address indexed from, address indexed to, uint256 value );
-
-	// This notifies clients about the amount burnt
-	event Burn( address indexed from, uint256 value );
+	// // This generates a public event on the blockchain that will notify clients
+	// event Transfer( address indexed from, address indexed to, uint256 value );
 
 	/**
 	 * 	Initializes contract with initial supply tokens to the creator of the contract
@@ -51,11 +78,6 @@ contract TokenERC20 {
 		// Asserts are used to use static analysis to find bugs in your code. They should never fail
 		assert( balanceOf[_from] + balanceOf[_to] == previousBalances );
 	}
-
-	function 	assertOverFlows() private {
-		// assert( balanceOf[_to] + _value > balanceOf[_to] ); // Check for overflows
-	}
-	
 
 	/**
 	* Transfer tokens
@@ -97,6 +119,7 @@ contract TokenERC20 {
 	* @param _spender The address authorized to spend
 	* @param _value the max amount they can spend
 	*/
+
 	function 	approve( address _spender, uint256 _value ) public returns ( bool success ) {
 		
 		allowance[msg.sender][_spender] = _value;
@@ -121,42 +144,5 @@ contract TokenERC20 {
 			spender.receiveApproval( msg.sender, _value, this, _extraData );
 			return true;
 		}
-	}
-
-	/**
-	* Destroy tokens
-	*
-	* Remove `_value` tokens from the system irreversibly
-	*
-	* @param _value the amount of money to burn
-	*/
-	function 	burn( uint256 _value ) public returns ( bool success ) {
-		
-		assert( balanceOf[msg.sender] >= _value );	// Check if the sender has enough
-		
-		balanceOf[msg.sender] -= _value;			// Subtract from the sender
-		totalSupply -= _value;						// Updates totalSupply
-		Burn( msg.sender, _value );
-		return true;
-	}
-
-	/**
-	* Destroy tokens from other account
-	*
-	* Remove `_value` tokens from the system irreversibly on behalf of `_from`.
-	*
-	* @param _from the address of the sender
-	* @param _value the amount of money to burn
-	*/
-	function 	burnFrom( address _from, uint256 _value ) public returns ( bool success ) {
-
-		assert( balanceOf[_from] >= _value );					// Check if the targeted balance is enough
-		assert( _value <= allowance[_from][msg.sender] );		// Check allowance
-
-		balanceOf[_from] -= _value;							// Subtract from the targeted balance
-		allowance[_from][msg.sender] -= _value;				// Subtract from the sender's allowance
-		totalSupply -= _value;								// Update totalSupply
-		Burn( _from, _value );
-		return true;
 	}
 }
