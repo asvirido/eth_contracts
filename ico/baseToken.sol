15c55c6d3217a12ad162c0bc72eff688c18c6f25
asvirido <trotillostudio@gmail.com>
2017-12-19 13:48:30 +0200

    deleted few files and fix style in baseToken

--- ico/baseToken.sol
+++ ico/baseToken.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.4.16;
 contract safeMath {
 
 	function add( uint256 x, uint256 y ) constant internal returns ( uint256 z ) {
+		
 		assert( ( z = x + y ) >= x );
 	}
 
 	function sub( uint256 x, uint256 y ) constant internal returns ( uint256 z ) {
+		
 		assert( ( z = x - y ) <= x );
 	}
 }
