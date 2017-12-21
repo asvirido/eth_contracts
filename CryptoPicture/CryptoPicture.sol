9bef405de6b8325c56eef666175b43c4f9843fca
asvirido <trotillostudio@gmail.com>
2017-12-21 16:07:26 +0200

    add

--- CryptoPicture/CryptoPicture.sol
+++ CryptoPicture/CryptoPicture.sol
@@ -10,9 +10,12 @@ contract CryptoPicture {
 	mapping ( bytes32 => string ) 	_authorCryptoPicture;
 	mapping ( bytes32 => bytes32 ) 	_hashPicture;
 	mapping ( bytes32 => address ) 	_ownerCryptoPicture;
-	
-	event 	Transfer( address from, address to, bytes32 picture );
 
+	mapping ( address => mapping ( address => mapping ( bytes32 => bool ) ) ) 	_allowance;
+
+	event 	Transfer( address from, address to, bytes32 picture );
+	event 	Approval( address owner, address spender, bytes32 cryptoPicture, bool resolution );
+	
 	function 	CryptoPicture() public {
 
 		_admin = msg.sender;
@@ -26,12 +29,13 @@ contract CryptoPicture {
 		_;
 	}
 
-	modifier 	assertOwnerPicture( uint id ) { 
+	modifier 	assertOwnerPicture( address owner, uint id ) { 
 
 		bytes32 hash;
 
+		assertId( id );
 		hash = _cryptoPicture[id];
-		if ( msg.sender != _ownerCryptoPicture[picture] ) {
+		if ( owner != _ownerCryptoPicture[picture] ) {
 			assert( false );
 		} 
 		_;
@@ -47,8 +51,8 @@ contract CryptoPicture {
 
 		bytes32  hash;
 
-		if ( id >= _supply || _noEmpty[id] == true )
-			assert( false );
+		assertId( id );
+		assertNoEmpty( id );
 
 		hash = sha256( this, id, namePicture, hashPicture, author, owner );
 
@@ -60,47 +64,77 @@ contract CryptoPicture {
 		_noEmpty[id] = true;
 	}
 
-	function 	transfer( address to, uint id ) public assertOwnerPicture( id ) {
-		
-		bytes32 hash;
-
-		hash = _cryptoPicture[id];
-
-		_ownerCryptoPicture[hash] = to;
-		Transfer( msg.sender, to, hash );
-	}
-
-	function 	getCryptoPicture( uint id ) public constant returns ( bytes32  hash ) {
-
-		if ( id >= supply )
-			assert( false );
-
-		hash = _cryptoPicture[id];
-	}
-	
-
-	function 	getTotalSupply() public constant returns ( uint supply )  {
-
+	function 	totalSupply() public constant returns ( uint supply )  {
 		supply = _supply;
 	}
 	
-	function 	getNamePicture( bytes32 picture ) public constant returns ( string name ) {
+	function 	allowance( address owner, address spender, uint id ) public constant returns ( bool ) {
+		bytes32 	picture;
 		
+		assertId( id );
+		picture = _cryptoPicture[id];
+		return 	_allowance[owner][spender][picture];
+	}
+
+	function 	approve( address spender, uint id, bool resolution ) public assertOwnerPicture( msg.sender, id ) returns ( bool ) {
+		bytes32 	hash;
+
+		hash = _cryptoPicture[id];
+		_allowance[msg.sender][spender][hash] = resolution;
+		Approval( msg.sender, spender, hash, resolution );
+		return true;
+	}
+
+	function 	transfer( address to, uint id ) public assertOwnerPicture( msg.sender, id ) returns ( bool ) {
+		bytes32 hash;
+
+		hash = _cryptoPicture[id];
+		_ownerCryptoPicture[hash] = to;
+		Transfer( msg.sender, to, hash );
+		return true;
+	}
+
+	function 	transferFrom( address from, address to, uint id ) public assertOwnerPicture( from, id ) returns( bool ) {
+		bytes32 	hash;
+
+		hash = _cryptoPicture[id];
+		if ( _allowance[from][msg.sender][hash] == false )
+			assert( false );
+		_ownerCryptoPicture[hash] = to;
+		Transfer( from, to, hash );
+		return true;
+	}
+	
+
+	function 	getCryptoPicture( uint id ) public constant returns ( bytes32  hash ) {
+		assertId( id );
+
+		hash = _cryptoPicture[id];
+	}
+	
+	function 	getNamePicture( bytes32 picture ) public constant returns ( string name ) {
 		name = _namePicture[picture];
 	}
 
 	function 	getAutorPicture( bytes32 picture ) public constant returns ( string autor ) {
-		
 		author = _authorCryptoPicture[picture];
 	}
 	
 	function 	getHashPicture( bytes32 picture ) public constant returns ( bytes32 hash ) {
-		
 		hash = _hashPicture[picture];
 	}
 
 	function 	getOwnerPicture( bytes32 picture ) public constant returns ( address owner ) {
-		
 		owner = _ownerCryptoPicture[picture];
 	}
+
+	function 	assertId( uint id ) private {		
+		if ( id >= _supply )
+			assert( false );
+	}
+
+	function 	assertNoEmpty( uint id ) private {
+		if (_noEmpty[id] == true )
+			assert( false );
+	}	
 }
