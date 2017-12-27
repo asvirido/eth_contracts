850d78fc4f325746dc94ec1a3c6c9e3fda4561dc
asvirido <trotillostudio@gmail.com>
2017-12-27 21:22:45 +0200

    new version interface admin

--- EscortService/escortService.sol
+++ EscortService/escortService.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.18;
 
 contract Admin {
 
-	address public 	_admin;
+	address private _admin;
 
 	function 	Admin( address admin ) public {
 		_admin = admin;
@@ -11,6 +11,17 @@ contract Admin {
 	function 	assertAdmin() private returns ( bool ) {
 		if ( _admin != msg.sender )
 			require( false );
+		return true;
+	}
+
+	function 	setAdmin( address admin ) 	private returns ( bool ) {
+		assertAdmin();
+		_admin = admin;
+		return true;
+	}
+
+	function 	getAdmin() public constant returns( address admin ) {
+		return _admin;
 	}
 }
 
--- interface/admin.sol
+++ interface/admin.sol
@@ -1,14 +1,24 @@
-
 contract Admin {
 
-	address public 	_admin;
+	address private _admin;
 
-	function 	Admin() public {
-		_admin = msg.sender;
+	function 	Admin( address admin ) public {
+		_admin = admin;
 	}
 
 	function 	assertAdmin() private returns ( bool ) {
 		if ( _admin != msg.sender )
 			require( false );
+		return true;
+	}
+
+	function 	setAdmin( address admin ) 	private returns ( bool ) {
+		assertAdmin();
+		_admin = admin;
+		return true;
+	}
+
+	function 	getAdmin() public constant returns( address admin ) {
+		return _admin;
 	}
 }
