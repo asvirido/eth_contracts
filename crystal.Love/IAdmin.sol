766a1ee94434d31e794551ac110ebd7eeec51c82
liderako <trotillostudio@gmail.com>
2018-01-17 22:19:23 +0200

    Update IAdmin.sol

--- crystal.Love/IAdmin.sol
+++ crystal.Love/IAdmin.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.4.18;
 
 contract IAdmin {
 	function 	assertAdmin() private returns ( bool );
-	function 	setAdmin( address admin ) public returns ( bool );
 	function 	getAdmin() public constant returns( address admin );
 
-	event 	NewAdmin( address _admin );
 }
