773d00fc9ad69196513bb523a6f2c5a9d7233d91
Anton Sviridov <asvirido@e1r6p16.unit.ua>
2018-01-18 13:34:14 +0200

    add burn in crystals.love

--- Crystals-Love/token.sol
+++ Crystals-Love/token.sol
@@ -4,8 +4,39 @@ import "browser/ERC20.sol";
 import "browser/Admin.sol";
 
 contract crystalLove is ERC20, Admin {
-	function crystalLove (string nameToken, string symbolToken, uint256 supply, uint8 decimals)
+	address public _crowdSale;
+
+	event Burn( address indexed from, uint256 value );
+
+	function 	crystalLove(string nameToken, string symbolToken, uint256 supply, uint8 decimals)
 		public 	ERC20( nameToken, symbolToken, supply, decimals )
 				Admin(msg.sender) {
 	}
+
+	function 	setAddressCrowdSale( address smartContract ) public returns ( bool success ) {
+		assertAdmin();
+
+		_crowdSale = smartContract;
+		retrun 	true;
+	}
+
+	function 	burn( uint256 value ) public returns ( bool success ) {	
+		require( _balanceOf[msg.sender] >= value );
+		require( msg.sender == _crowdSale );
+		
+		_balanceOf[msg.sender] -= value;
+		_totalSupply -= value;
+
+		Burn( msg.sender, value );
+		return true;
+	}
 }
+
+// contract contractName {
+
+// 	uint public deadLine;
+// 	function contractName (uint howLong) {
+// 		deadline = now + howLong * 1 minutes;
+// 	}	
+// }
+
