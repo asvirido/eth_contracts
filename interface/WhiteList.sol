0377cc78b686864abf1bfc4b98af9d6e0e6ce0ba
liderako <trotillostudio@gmail.com>
2018-02-02 14:20:14 +0200

    Create WhiteList.sol
    Функционал для белого листа. Тут Админ может удалять и добавлять новых модераторов. Модераторы в свою очередь могут авторизировать пользователей.

---
+++ interface/WhiteList.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.4.18;
+
+contract Admin {
+
+	address private _admin;
+
+	function 	Admin( address admin ) public {
+		_admin = admin;
+	}
+
+	function 	assertAdmin() internal view returns ( bool ) {
+		if ( _admin != msg.sender )
+			require( false );
+		return true;
+	}
+
+	function 	getAdmin() public constant returns( address admin ) {
+		return 	_admin;
+	}
+}
+
+contract 	WhiteList is Admin {
+	mapping ( address => bool ) internal _moderator;
+	mapping ( address => bool ) internal _listAuthorizedUser;
+
+	/* + */
+	function 	WhiteList( address admin ) public Admin( admin ) { }
+	/* + */
+	function 	setAuthorizeUser( address user ) public {	
+		assertModerator();
+		_listAuthorizedUser[user] = true;
+	}
+	/* + */
+	function 	addModerator( address userModerator ) public {
+		assertAdmin();
+		_moderator[userModerator] = true;
+	}
+	/* + */
+	function 	deleteModerator( address userModerator ) public {
+		assertAdmin();
+		_moderator[userModerator] = false;
+	}
+	/* + */
+	function 	getAuthorizeUser( address user ) public constant returns( bool ) {
+		return 	_listAuthorizedUser[user];
+	}
+	/* + */
+	function 	getModerator( address user ) public constant returns( bool ) {
+		return 	_moderator[user];
+	}
+	/* + */
+	function 	assertModerator() view internal {
+		if ( _moderator[msg.sender] == false )
+			require( false );
+	}
+	/* + */
+	function 	assertUserAuthorized( address user ) view internal {
+		if ( _listAuthorizedUser[user] == false )
+			require( false );
+	}
+}
