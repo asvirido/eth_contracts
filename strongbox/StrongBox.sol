cb45420b9376b88ca8c60fba74149137963c9b68
Anton Sviridov <asvirido@e1r6p1.unit.ua>
2018-02-22 20:02:32 +0200

    delete event Transfer

--- strongbox/StrongBox.sol
+++ strongbox/StrongBox.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.18;
 contract Token {
 	function transfer( address _to, uint256 _value ) public returns ( bool success );
 	function transferFrom( address _from, address _to, uint256 _value ) public returns ( bool success );
-	event Transfer( address indexed _from, address indexed _to, uint256 _value );
 }
 
 contract SafeMath {
