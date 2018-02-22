6e42ebcc195673f19c5178eb9fab4d3d90cc7ae0
Anton Sviridov <asvirido@e1r6p1.unit.ua>
2018-02-22 20:01:49 +0200

    update StrongBox

--- strongbox/StrongBox.sol
+++ strongbox/StrongBox.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.4.18;
 
+contract Token {
+	function transfer( address _to, uint256 _value ) public returns ( bool success );
+	function transferFrom( address _from, address _to, uint256 _value ) public returns ( bool success );
+	event Transfer( address indexed _from, address indexed _to, uint256 _value );
+}
+
 contract SafeMath {
-	function safeSub( uint a, uint b ) internal returns ( uint ) {
-
-		assert( b <= a );
-		return a - b;
-	}
-
-	function safeAdd( uint a, uint b ) internal returns ( uint ) {
-
+	function safeAdd( uint a, uint b ) internal pure returns ( uint ) {
 		uint 	c;
 
 		c = a + b;
@@ -18,17 +17,17 @@ contract SafeMath {
 }
 
 contract StrongBox is SafeMath {
-	mapping ( bytes32 => mapping (address => uint) ) _tokens;
-
+	mapping ( bytes32 => mapping ( address => uint ) ) _tokens;
+	/* - */
 	function () public payable {
 		require( false );
 	}
-
-	function 	depositEth( bytes32 hash) public payable {
+	/* - */
+	function 	depositEth( bytes32 hash ) public payable {
 		assertQuantity( msg.value );
-		_tokens[hash][0] = msg.value;
+		_tokens[hash][0] = safeAdd( _tokens[hash][0], msg.value );
 	}
-
+	/* - */
 	function 	withdrawEth( string key ) public {
 		bytes32 	hash;
 		uint		amount;
@@ -39,13 +38,16 @@ contract StrongBox is SafeMath {
 		_tokens[hash][0] = 0;
 		msg.sender.transfer( amount );
 	}
-
+	/* - */
 	function 	depositToken( bytes32 hash, address token, uint amount ) public {
 		assertQuantity( amount );
 		assertToken( token );
-		_tokens[hash][token] = amount;
+		_tokens[hash][token] = safeAdd( _tokens[hash][token], amount );
+		if ( Token( token ).transferFrom( msg.sender, this, amount ) == false ) {
+			assert( false );
+		}
 	}
-
+	/* - */
 	function 	withdrawToken( string key, address token ) public {
 		uint 		amount;
 		bytes32		hash;
@@ -54,14 +56,31 @@ contract StrongBox is SafeMath {
 		hash = sha256( key );
 		amount = _tokens[hash][token];
 		assertQuantity(amount);
-		_tokens[hash][token] = 0
+		_tokens[hash][token] = 0;
 		if ( Token( token ).transfer( msg.sender, amount ) == false ) {
 			assert( false );
 		}
 	}
+	/* - */
+	function	transferBox( string keyTo, address token, bytes32 from ) public {
+		uint 		amount;
+		bytes32		hash;
+
+		hash = sha256( keyTo );
+		assertDoubleHash(hash, from);
+		amount = _tokens[hash][token];
+		assertQuantity( amount );
+		_tokens[hash][token] = 0;
+		_tokens[from][token] = safeAdd( _tokens[from][token], amount );
+	}
 
 	function 	assertQuantity( uint amount ) private pure {
 		if ( amount == 0 ) {
+			require( false );
+		}
+	}
+	function 	assertDoubleHash( bytes32 a, bytes32 b) private pure {
+		if ( a == b ) {
 			require( false );
 		}
 	}
